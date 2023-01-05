ef3db73be73ff7d80306206de7b26029a51a790a
Alex Mantovani <mantovani.alex@gmail.com>
2023-01-05 13:46:15 +0100

    pulizia

--- cropper.py
+++ cropper.py
@@ -1,6 +1,4 @@
 import argparse
-# import sys
-import datetime
 
 from PyPDF2 import PdfWriter, PdfReader
 from PyPDF2.generic import (
@@ -15,24 +13,20 @@ def rect_inset(page, offset):
     return page
 
 
-# if len(sys.argv) <= 1:
-#     print('Argoomenti mancanti.\nUso: python3 cropper.py "<pdf file>" "<pdf file>" ...')
-#     raise SystemExit(2)
-
-parser = argparse.ArgumentParser(description='Description of your program')
+parser = argparse.ArgumentParser(description='An utility to merge and crop your PDF files')
 parser.add_argument('-f','--files', help='File or files to merge and crop', required=True, nargs='+')
 parser.add_argument('-o','--output', help='Output file', required=False, default='result.pdf')
+parser.add_argument('-c','--crop', help='Crop size (0 = no crop)', required=False, default='50')
 args = vars(parser.parse_args())
 
 writer = PdfWriter()
-# timestamp = int(datetime.datetime.now().timestamp())
 
-for index, file_name in enumerate(args['files']):
+for file_name in args['files']:
     reader = PdfReader(file_name)
 
     for page in reader.pages:
-        page.cropbox.upper_left = (0, 0)
-        writer.add_page(rect_inset(page, 50))
+        # page.cropbox.upper_left = (0, 0)
+        writer.add_page(rect_inset(page, args['crop']))
 
     with open(args['output'], 'wb') as fp:
         print(f"Write file {args['output']}")
