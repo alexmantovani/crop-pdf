57a0e361bb748e76a22cdb18937d212c83df51ae
Alex Mantovani <mantovani.alex@gmail.com>
2023-01-06 18:51:27 +0100

    clean

--- cropper.py
+++ cropper.py
@@ -19,10 +19,8 @@ parser = argparse.ArgumentParser(description='An utility to merge and crop your 
 parser.add_argument('-f','--files', help='File or files to merge and crop', required=True, nargs='+')
 parser.add_argument('-o','--output', help='Output file', required=False, default='result.pdf')
 parser.add_argument('-c','--crop', help='Crop size (0 = no crop)', required=False, default='50', type=int)
-# parser.add_argument('-no_ss','--no_swift_scan', help='Remove SwiftScan edge', required=False, action='no_swift_scan')
 parser.add_argument('--no_swift_scan', action=argparse.BooleanOptionalAction)
 args = vars(parser.parse_args())
-print(args)
 
 crop = args['crop']
 if args['no_swift_scan'] is True:
@@ -34,7 +32,6 @@ for file_name in args['files']:
     reader = PdfReader(file_name)
 
     for page in reader.pages:
-        # page.cropbox.upper_left = (0, 0)
         writer.add_page(rect_inset(page, crop))
 
     with open(args['output'], 'wb') as fp:
